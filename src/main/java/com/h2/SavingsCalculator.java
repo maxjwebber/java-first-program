93fd39e9a9e7daab8e4e7f7bde8d31844fca3840
Max <max@maxwrit.es>
2021-12-15 17:21:24 -0500

    added validation for parsing numbers from strings

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -53,12 +53,12 @@ public class SavingsCalculator {
 
         for(int i=0; i<creditsAsString.length; i++)
         {
-            credits[i] = Float.parseFloat(creditsAsString[i]);
+            credits[i] = Utilities.getFloatValue(creditsAsString[i]);
         }
 
         for(int i=0; i<debitsAsString.length; i++)
         {
-            debits[i] = Float.parseFloat(debitsAsString[i]);
+            debits[i] = Utilities.getFloatValue(debitsAsString[i]);
         }
 
         SavingsCalculator calculator = new SavingsCalculator(credits,debits);
